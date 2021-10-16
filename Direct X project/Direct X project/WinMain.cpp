e33498316268ed8a37f56ba3330e95e95235fbe0
Matt <Matt.j.bowes@gmail.com>
2021-10-16 20:51:14 +0100

    Windows Messaging Loop
    Created a small message pump that listens for inputs, it then translates and dispatches that message.
    
    also created a switch statement for if that message is 'close down window', a character, or the left mouse button being pressed down.

--- Direct X project/Direct X project/WinMain.cpp
+++ Direct X project/Direct X project/WinMain.cpp
@@ -1,4 +1,42 @@
 #include <Windows.h>
+#include <iostream>
+#include <sstream>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+		//on close window
+		case WM_CLOSE:
+		{
+			PostQuitMessage(000);
+			break;
+		}
+
+		//keyboard input
+		case WM_CHAR:
+		{
+			//clears and appends windows title to user input
+			static std::string title;
+			title.push_back((char)wParam);
+			SetWindowText(hWnd, title.c_str());
+			break;
+		}
+		
+		//mouse input
+		case WM_LBUTTONDOWN:
+		{
+			//changes windows title to mouse coords
+			const POINTS pt = MAKEPOINTS(lParam);
+			std::ostringstream oss;
+			oss << "(" << pt.x << "," << pt.y << ")";
+			SetWindowText(hWnd, oss.str().c_str());
+			break;
+		}
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
 
 int CALLBACK WinMain(
 	HINSTANCE	hInstance,
@@ -6,5 +44,52 @@ int CALLBACK WinMain(
 	LPSTR		lpCmdLine,
 	int			nCmdShow)
 {
+	const auto pClassName = "teehee";
+
+	// register windows class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	//create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"DirectX Window :D",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr);
+	//show the window 
+	ShowWindow(hWnd, SW_SHOW);
+	
+	//message pump
+	MSG msg;
+	BOOL getResult;
+
+	while ((getResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (getResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 	return 0;
 }
