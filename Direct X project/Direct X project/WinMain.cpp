06fa0b4f80a6a780f4894b38d7af0c576e6553cb
Matt <Matt.j.bowes@gmail.com>
2021-10-17 21:11:35 +0100

    Windows Classes
    Major overhaul to how windows are created:
    New Window header that contains a 2 classes, one nested within another
    
    'WindowClass' handles the Instance of the window, and 'window' handles the windows API side of the program.
    
    Window.cpp handles everything to do with the instance of the window.

--- Direct X project/Direct X project/WinMain.cpp
+++ Direct X project/Direct X project/WinMain.cpp
@@ -1,42 +1,5 @@
-#include <Windows.h>
-#include <iostream>
-#include <sstream>
+#include "Window.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	switch (msg)
-	{
-		//on close window
-		case WM_CLOSE:
-		{
-			PostQuitMessage(000);
-			break;
-		}
-
-		//keyboard input
-		case WM_CHAR:
-		{
-			//clears and appends windows title to user input
-			static std::string title;
-			title.push_back((char)wParam);
-			SetWindowText(hWnd, title.c_str());
-			break;
-		}
-		
-		//mouse input
-		case WM_LBUTTONDOWN:
-		{
-			//changes windows title to mouse coords
-			const POINTS pt = MAKEPOINTS(lParam);
-			std::ostringstream oss;
-			oss << "(" << pt.x << "," << pt.y << ")";
-			SetWindowText(hWnd, oss.str().c_str());
-			break;
-		}
-	}
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 int CALLBACK WinMain(
 	HINSTANCE	hInstance,
@@ -44,35 +7,7 @@ int CALLBACK WinMain(
 	LPSTR		lpCmdLine,
 	int			nCmdShow)
 {
-	const auto pClassName = "teehee";
-
-	// register windows class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
-
-	//create window instance
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		"DirectX Window :D",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr);
-	//show the window 
-	ShowWindow(hWnd, SW_SHOW);
-	
+	Window WNDCLASS(400, 400, "Window Class revision (:");
 	//message pump
 	MSG msg;
 	BOOL getResult;
@@ -87,9 +22,6 @@ int CALLBACK WinMain(
 	{
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
-	return 0;
+
+	return msg.wParam;
 }
---
+++ Direct X project/Direct X project/Window.cpp
@@ -0,0 +1,125 @@
+#include "Window.h"
+#include <iostream>
+#include <sstream>
+
+
+Window::WindowClass Window::WindowClass::wndClass;
+
+Window::WindowClass::WindowClass() noexcept:
+	hInst(GetModuleHandle(nullptr))
+{
+	// register windows class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = HandleMsgSetup;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = GetInstance();
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = GetName();
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+}
+
+Window::WindowClass::~WindowClass()
+{
+	UnregisterClass(wndClassName, GetInstance());
+}
+
+const char* Window::WindowClass::GetName() noexcept
+{
+	return wndClassName;
+}
+HINSTANCE Window::WindowClass::GetInstance() noexcept
+{
+	return wndClass.hInst;
+}
+
+Window::Window(int width, int height, const char* name) noexcept
+{
+	//values for calculating windows size based on the client area of the window
+	RECT wr;
+	wr.left = 100;
+	wr.right = width + wr.left;
+	wr.top = 100;
+	wr.bottom = height + wr.top;
+
+	std::printf("build");
+
+	AdjustWindowRect(&wr, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE);
+	hWnd = CreateWindow(
+		WindowClass::GetName(), name,
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		CW_USEDEFAULT, CW_USEDEFAULT, wr.right - wr.left, wr.bottom - wr.top,
+		nullptr, nullptr, WindowClass::GetInstance(), this);
+
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+}
+
+Window::~Window()
+{
+	DestroyWindow(hWnd);
+}
+
+LRESULT CALLBACK Window::HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
+{
+	if (msg == WM_NCCREATE)
+	{
+		const CREATESTRUCTW* const pCreate = reinterpret_cast<CREATESTRUCTW*>(lParam);
+		Window* const pWnd = static_cast<Window*>(pCreate->lpCreateParams);
+		SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pWnd));
+		SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(&Window::HandleMsgThunk));
+		return pWnd->HandleMsg(hWnd, msg, wParam, lParam);
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+LRESULT CALLBACK Window::HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
+{
+	Window* const pWnd = reinterpret_cast<Window*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+	return pWnd->HandleMsg(hWnd, msg, wParam, lParam);
+}
+
+LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
+{
+	switch (msg)
+	{
+		//on close window
+		case WM_CLOSE:
+		{
+			printf("ding");
+			PostQuitMessage(000);
+			printf("dong");
+			return 0;
+
+		}
+
+		//keyboard input
+		case WM_CHAR:
+		{
+			//clears and appends windows title to user input
+			static std::string title;
+			title.push_back((char)wParam);
+			SetWindowText(hWnd, title.c_str());
+			break;
+		}
+
+		//mouse input
+		case WM_LBUTTONDOWN:
+		{
+			//changes windows title to mouse coords
+			const POINTS pt = MAKEPOINTS(lParam);
+			std::ostringstream oss;
+			oss << "(" << pt.x << "," << pt.y << ")";
+			SetWindowText(hWnd, oss.str().c_str());
+			break;
+		}
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
